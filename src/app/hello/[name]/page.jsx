6a2b2d68640bfff1356fae073e8a16a36d3f19bf
aouad <aouadmouhamed1@gmail.com>
2025-06-22 01:29:49 +0100

    not found page

--- src/app/hello/[name]/page.jsx
+++ src/app/hello/[name]/page.jsx
@@ -1,7 +1,9 @@
 
 import React from 'react'
 
-export default function helloName({params}) {
+export default function helloName({params , searchParams}) {
+  console.log(params , searchParams);
+  
   return (
     <div>
         <h1>hello {params.name}</h1>
